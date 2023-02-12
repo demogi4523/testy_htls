f1bdac34f716b9a255ce313cb2d3d52435fa53b7
demogi4523 <demogi4523@yandex.ru>
2023-02-12 21:36:19 +0800

    (feat): Solve 3 programming task

---
+++ 3.js
@@ -0,0 +1,32 @@
+// Написать метод/функцию, который/которая на вход принимает число (int), 
+// а на выходе выдает слово “компьютер” в падеже, соответствующем указанному количеству. 
+// Например, «25 компьютеров», «41 компьютер», «1048 компьютеров».
+
+export default function (computer_amount) {
+  if (computer_amount < 0) {
+    throw new Error("Компьютеров не может быть меньше 0:)");
+  }
+  let res;
+  const reminder = computer_amount % 10;
+  switch (reminder) {
+    case 1:  // if (x === 'value1')
+      {
+        res = "1 компьютер";
+        break;
+      }
+    case 2:  // if (x === 'value2')
+    case 3:  // if (x === 'value2')
+    case 4:  // if (x === 'value2')
+      {
+        res = computer_amount + " компьютера";
+        break;
+      }
+    default:
+      {
+        res = computer_amount + " компьютеров";
+        break;
+      }
+  }
+
+  return res;
+}
